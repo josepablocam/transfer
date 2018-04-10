d83aa7bc16fbd79a35ebf75058bad17a8f036960
Jose Cambronero <jcamsan@mit.edu>
2018-04-10 17:20:39 -0400

    make use of memory update refinement in graph builder

--- manual/run_kaggle_program.sh
+++ manual/run_kaggle_program.sh
@@ -4,6 +4,8 @@ echo "Running script $1"
     mkdir -p generated/;
     echo 'Rewriting'
     python -m plpy.rewrite.expr_lifter $1.py generated/$1_lifted.py;
-    echo 'Running and Tracing'
-    ipython3 -m plpy.analyze.dynamic_tracer -- generated/$1_lifted.py generated/$1_tracer.pkl --loop_bound 2 --log $1.log
+    echo 'Running and Tracing';
+    ipython3 -m plpy.analyze.dynamic_tracer -- generated/$1_lifted.py generated/$1_tracer.pkl --loop_bound 2 --log $1.log;
+    echo 'Constructing graph';
+    python -m plpy.analyze.graph_builder generated/$1_tracer.pkl generated/$1_graph.pkl --ignore_unknown --memory_refinement 1;
 )
