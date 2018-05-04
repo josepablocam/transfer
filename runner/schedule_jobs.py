82981066f14143512a5476450bced10556d5bc0d
Jose Cambronero <jcamsan@mit.edu>
2018-05-04 11:06:24 -0400

    more work

--- runner/schedule_jobs.py
+++ runner/schedule_jobs.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 import os
+import re
 import subprocess
 import tempfile
 
@@ -12,6 +13,7 @@ def build_docker_command(docker_image, timeout, script_path, vm_output_dir, dock
     docker_command = [
         'docker',
         'run',
+        '--rm',
         '--name', container_name,
         '-v', '{}:{}'.format(vm_output_dir, docker_output_dir),
         '--memory', mem_limit,
@@ -27,6 +29,7 @@ def schedule(command):
     script_file =  tempfile.NamedTemporaryFile(suffix='.sh', delete=False)
     if not isinstance(command, str):
         command = ' '.join(command)
+    print('Scheduling: {}'.format(command))
     script_file.write('#!/bin/bash\n')
     script_file.write('{}\n'.format(command))
     script_file.flush()
@@ -40,11 +43,13 @@ def setup_at_config(load_avg):
     load_avg_cmd = ['atd', '-l', str(load_avg)]
     return subprocess.call(load_avg_cmd)
 
-def schedule_jobs(docker_image, scripts_dir, vm_output_dir, docker_output_dir, timeout, load_avg, mem_limit):
+def schedule_jobs(docker_image, scripts_dir, vm_output_dir, docker_output_dir, timeout, load_avg, mem_limit, regex_pattern):
     setup_at_config(load_avg)
     scripts = [s for s in os.listdir(scripts_dir) if s.split('.')[-1] == 'py']
     for script_name in scripts:
         script_path = os.path.join(scripts_dir, script_name)
+        if regex_pattern is not None and re.match(regex_pattern, script_path) is None:
+            continue
         cmd = build_docker_command(docker_image, timeout, script_path, vm_output_dir, docker_output_dir, mem_limit)
         schedule(cmd)
 
@@ -56,7 +61,8 @@ def main(args):
         args.docker_output_dir,
         args.timeout,
         args.load_average,
-        args.mem_limit
+        args.mem_limit,
+        args.regex
     )
 
 if __name__ == '__main__':
@@ -68,6 +74,7 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--mem_limit', type=str, help='Maximum memory per docker container', default='20GB')
     parser.add_argument('-t', '--timeout', type=str, help='Timeout per tracing portion', default='2h')
     parser.add_argument('-l', '--load_average', type=float, help='Load average for atd command', default=5.0)
+    parser.add_argument('-r', '--regex', type=str, help='Only schedule scripts with a path that matches the regex', default=None)
     args = parser.parse_args()
     try:
         main(args)
