4156a84d9f9ea70014b83e29f5ea407c0eb0c63f
Jose Cambronero <jcamsan@mit.edu>
2018-05-10 16:56:31 -0400

    added some work on ranking functions

--- transfer/rank_functions.py
+++ transfer/rank_functions.py
@@ -1,7 +1,12 @@
 from abc import ABC, abstractmethod
 import ast
+
 import editdistance
 import networkx as nx
+import numpy as np
+import py2neo
+import sklearn.feature_extraction
+import tqdm
 import zss
 
 from .lift_donations import DonatedFunction
@@ -28,6 +33,7 @@ class FunctionDistanceComputer(ABC):
 
 
 # TODO we should probably handle commutativity ourselves (currently ignores that)
+# TODO: this is uber slow...consider PQ-GRAM instead, if we want AST-based options
 class ZhangShashaTreeDistance(FunctionDistanceComputer):
     @staticmethod
     def get_node_label(node):
@@ -71,58 +77,69 @@ class ZhangShashaTreeDistance(FunctionDistanceComputer):
             self.label_dist
             )
 
+# TODO: WRITE THIS AT SOME POINT....
+# class DependencyGraphDistance(FunctionDistanceComputer):
+#     @staticmethod
+#     def get_graph(elem):
+#         if isinstance(elem, DonatedFunction):
+#             return elem.graph
+#         elif isinstance(elem, nx.DiGraph):
+#             return elem
+#         else:
+#             raise ValueError('Unhandled function type {} for graph distance'.format(type(elem)))
+#
+#     def _build_dag(self, graph_1, graph_2, costs):
+#         # construct DAG by pairing nodes such that
+#         # sum of distances for paired nodes + sum of delete nodes is
+#         # minimized
+#         # graph_1 and graph_2 are DAGS by definition
+#         # so we only add edges from graph_1 to graph_2, so this is still a DAG
+#         # and we want no cross-graph edges to cross (indicating different data flow)
+#         # objective:
+#         # min sum_{n_1 in graph_1} sum_{n_2 in graph_2} cost[(n_1, n_2)] * chosen((n_1, n_2)) +
+#         #     sum_{n_1 in graph_1} cost[n_1] * (1 - chosen(n_1)) +
+#         #     sum_{n_2 in graph_2} cost[n_2] * (1 - chosen(n_2))
+#         # subject to
+#         # each n_1, n_2 can be chosen at most once
+#         # # no crossing edges
+#         # for all n_1 in graph_1:
+#         #   for all pred in predecessors(n_1):
+#         #       edge_from(p).to <= edge_from(n_1).to
+#         return cost
+#
+#     def _distance(self, function_1, function_2):
+#         raise NotImplementedError('jose is still working on this')
+#
+#         graph_1 = self.get_graph(function_1)
+#         graph_2 = self.get_graph(function_2)
+#
+#         # cost for pairing up each possible combo
+#         node_distance_computer = ZhangShashaTreeDistance()
+#         costs = {}
+#         for node_id_1, node_data_1 in graph_1.nodes(data=True):
+#             for node_id_2, node_data_2 in graph_2.nodes(data=True):
+#                 dist = node_distance_computer.distance(node_data_1['line'], node_data_2['line'])
+#                 costs[(node_id_1, node_id_2)] = dist
+#
+#         # cost for just removing a node altogether
+#         for node_id, node_data in graph_1.nodes(data=True) + graph_2.nodes(data=True):
+#             costs[node_id] = node_distance_computer(node_data['line'], '')
+#
+#         return self._cost_build_dag(graph_1, graph_2, costs)
 
-class DependencyGraphDistance(FunctionDistanceComputer):
-    @staticmethod
-    def get_graph(elem):
-        if isinstance(elem, DonatedFunction):
-            return elem.graph
-        elif isinstance(elem, nx.DiGraph):
-            return elem
-        else:
-            raise ValueError('Unhandled function type {} for graph distance'.format(type(elem)))
 
-    def _build_dag(self, graph_1, graph_2, costs):
-        # construct DAG by pairing nodes such that
-        # sum of distances for paired nodes + sum of delete nodes is
-        # minimized
-        # graph_1 and graph_2 are DAGS by definition
-        # so we only add edges from graph_1 to graph_2, so this is still a DAG
-        # and we want no cross-graph edges to cross (indicating different data flow)
-        # objective:
-        # min sum_{n_1 in graph_1} sum_{n_2 in graph_2} cost[(n_1, n_2)] * chosen((n_1, n_2)) +
-        #     sum_{n_1 in graph_1} cost[n_1] * (1 - chosen(n_1)) +
-        #     sum_{n_2 in graph_2} cost[n_2] * (1 - chosen(n_2))
-        # subject to
-        # each n_1, n_2 can be chosen at most once
-        # # no crossing edges
-        # for all n_1 in graph_1:
-        #   for all pred in predecessors(n_1):
-        #       edge_from(p).to <= edge_from(n_1).to
-        return cost
+class SourceCodeStringDistance(FunctionDistanceComputer):
+    def __init__(self, str_dist_fun=None):
+        super().__init__()
+        if str_dist_fun is None:
+            str_dist_fun = editdistance.eval
+        if isinstance(str_dist_fun, str):
+            if str_dist_fun == 'levenshtein':
+                str_dist_fun = editdistance.eval
+            else:
+                raise ValueError('Unknown string distance name')
+        self.str_dist_fun = str_dist_fun
 
-    def _distance(self, function_1, function_2):
-        raise NotImplementedError('jose is still working on this')
-
-        graph_1 = self.get_graph(function_1)
-        graph_2 = self.get_graph(function_2)
-
-        # cost for pairing up each possible combo
-        node_distance_computer = ZhangShashaTreeDistance()
-        costs = {}
-        for node_id_1, node_data_1 in graph_1.nodes(data=True):
-            for node_id_2, node_data_2 in graph_2.nodes(data=True):
-                dist = node_distance_computer.distance(node_data_1['line'], node_data_2['line'])
-                costs[(node_id_1, node_id_2)] = dist
-
-        # cost for just removing a node altogether
-        for node_id, node_data in graph_1.nodes(data=True) + graph_2.nodes(data=True):
-            costs[node_id] = node_distance_computer(node_data['line'], '')
-
-        return self._cost_build_dag(graph_1, graph_2, costs)
-
-
-class LevenshteinStringDistance(FunctionDistanceComputer):
     @staticmethod
     def get_string(elem):
         if isinstance(elem, DonatedFunction):
@@ -137,16 +154,83 @@ class LevenshteinStringDistance(FunctionDistanceComputer):
     def _distance(self, function_1, function_2):
         src_1 = self.get_string(function_1)
         src_2 = self.get_string(function_2)
-        return editdistance.eval(src_1, src_2)
+        return self.str_dist_fun(src_1, src_2)
+
+class LearningDataPreparer(ABC):
+    def prepare(self, db):
+        # takes database
+        # returns a matrix and target vector to learn
+        pass
 
 
-# class FunctionAbstractor(ABC):
-#     """
-#     Abstract source code of a function
-#     """
-#     @abstractmethod
-#     def abstract(self, _function):
-#         pass
+class BinaryRelationshipDataPreparer(LearningDataPreparer):
+    def __init__(self, db, distance_computer=None):
+        self.db = db
+        if distance_computer is None:
+            # choose fastets option for now...
+            distance_computer = SourceCodeStringDistance('levenshtein')
+        self.distance_computer = distance_computer
+        self.relationships_and_features_cache = {}
+        self.vectorizer = sklearn.feature_extraction.DictVectorizer()
+
+    def _get_relationships_and_features(self, node, prefix=None):
+        result = self.relationships_and_features_cache.get(node, None)
+        if result is None:
+            # query database and get relations from that...
+            # no need to go through this again
+            rels = self.db.get_extracted_function_relationships_from_node(node)
+            feats = self._relationships_to_dict(rels, prefix)
+            result = (rels, feats)
+            self.relationships_and_features_cache[node] = result
+        rels, feats = result
+        return rels, dict(feats)
+
+    def _relationships_to_dict(self, relationships, prefix=None):
+        assert prefix is not None
+        if prefix is None:
+            prefix = ''
+        else:
+            prefix = prefix + '+'
+        _dict = {}
+        for relationship_type, val in relationships:
+            _dict[prefix + relationship_type.name + '+' + val] = True
+        return _dict
+
+    def prepare(self, remove_nodes=None):
+        self.db.startup()
+        X_data = []
+        y_data = []
+        # construct data matrix over all functions we extracted and stored in db
+        extracted_fun_nodes = self.db.extracted_functions()
+
+        for query_node in tqdm.tqdm(extracted_fun_nodes):
+            query_rels, query_feats = self._get_relationships_and_features(query_node, prefix='query-node')
+
+            query_results = self.db.query_by_relationships(query_rels)
+            query_fun = self.db.get_function_from_node(query_node)
+
+            # remove self from results
+            query_results.remove(query_node)
+            # drop other nodes (may be useful for train/test setups)
+            if remove_nodes:
+                query_results = query_results.difference(remove_nodes)
+
+            for result_node in tqdm.tqdm(query_results):
+                result_rels, result_feats = self._get_relationships_and_features(result_node, prefix='result-node')
+                result_fun = self.db.get_function_from_node(result_node)
+
+                row = {}
+                row.update(query_feats)
+                row.update(result_feats)
+                X_data.append(row)
+
+                dist = self.distance_computer.distance(query_fun, result_fun)
+                y_data.append(dist)
+
+        X_matrix = self.vectorizer.fit_transform(X_data)
+        y_vec = np.array(y_data)
+        return X_matrix, y_vec
+
 
 #
 # class FunctionRanker(ABC):
