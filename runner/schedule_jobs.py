698454e9fa94d5d37294f968dc09ff9ec21efb76
Jose Cambronero <jcamsan@mit.edu>
2018-05-11 15:52:42 -0400

    allow option to run without --rm when scheduling containers

--- runner/schedule_jobs.py
+++ runner/schedule_jobs.py
@@ -4,7 +4,15 @@ import re
 import subprocess
 import tempfile
 
-def build_docker_command(docker_image, timeout, script_path, vm_output_dir, docker_output_dir, mem_limit, plain):
+def build_docker_command(
+    docker_image,
+    timeout,
+    script_path,
+    vm_output_dir,
+    docker_output_dir,
+    mem_limit,
+    keep,
+    plain):
     # to be able to mount volumes, must use absolute path
     vm_output_dir = os.path.abspath(vm_output_dir)
     # TODO: we should probably fix this
@@ -13,10 +21,13 @@ def build_docker_command(docker_image, timeout, script_path, vm_output_dir, dock
     docker_command = [
         'docker',
         'run',
-        '--rm',
         '--name', container_name,
         '-v', '{}:{}'.format(vm_output_dir, docker_output_dir),
-        '--memory', mem_limit,
+        '--memory', mem_limit
+        ]
+    if not keep:
+        docker_command += ['--rm']
+    docker_command += [
         docker_image,
         timeout, script_path, docker_output_dir
     ]
@@ -41,13 +52,13 @@ def schedule(command):
     os.remove(script_file.name)
     return return_code
 
-def schedule_jobs(docker_image, scripts_dir, vm_output_dir, docker_output_dir, timeout, mem_limit, regex_pattern, plain):
+def schedule_jobs(docker_image, scripts_dir, vm_output_dir, docker_output_dir, timeout, mem_limit, keep, regex_pattern, plain):
     scripts = [s for s in os.listdir(scripts_dir) if s.split('.')[-1] == 'py']
     for script_name in scripts:
         script_path = os.path.join(scripts_dir, script_name)
         if regex_pattern is not None and re.match(regex_pattern, script_path) is None:
             continue
-        cmd = build_docker_command(docker_image, timeout, script_path, vm_output_dir, docker_output_dir, mem_limit, plain)
+        cmd = build_docker_command(docker_image, timeout, script_path, vm_output_dir, docker_output_dir, mem_limit, keep, plain)
         schedule(cmd)
 
 def main(args):
@@ -58,6 +69,7 @@ def main(args):
         args.docker_output_dir,
         args.timeout,
         args.mem_limit,
+        args.keep,
         args.regex,
         args.plain
     )
@@ -70,6 +82,7 @@ if __name__ == '__main__':
     parser.add_argument('docker_output_dir', type=str, help='Path in docker container to save any outputs generated')
     parser.add_argument('-m', '--mem_limit', type=str, help='Maximum memory per docker container', default='20GB')
     parser.add_argument('-t', '--timeout', type=str, help='Timeout per tracing portion', default='4h')
+    parser.add_argument('-k', '--keep', action='store_true', help='Keep container fs (i.e. run w/o --rm)')
     parser.add_argument('-r', '--regex', type=str, help='Only schedule scripts with a path that matches the regex', default=None)
     parser.add_argument('-p', '--plain', action='store_true', help='Run program as plain ipython (with timeout) and store stdout')
     args = parser.parse_args()
